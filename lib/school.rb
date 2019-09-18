f345d7a7bf72906ff937bc08f9a78d82a3753bfa
Jamal Frank <jamaljfrank@gmail.com>
2019-09-18 00:19:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,8 @@ class School
   end
   
   def add_student(student_name, student_grade)
-    roster[student_grade] = [] if !@roster[student_grade]
+    roster[student_grade] = []
+    if !@roster[student_grade]
     roster[student_grade] << student_name
 
 end
